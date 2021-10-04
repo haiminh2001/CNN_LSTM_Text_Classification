fee0a48b5db7ef6ec9dc9f4c7d8a08eed0ab816b
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-10-04 22:22:56 +0700

    upgrade doc2vec

--- TopicAllocate.py
+++ TopicAllocate.py
@@ -2,6 +2,7 @@ import nltk
 from keras.preprocessing.text import Tokenizer
 from gensim.models import Word2Vec
 import re
+import numpy as np 
 
 def preprocess(text):
     text = text.lower() # Lowercase
@@ -10,64 +11,55 @@ def preprocess(text):
     return text.strip()
 
 nltk.download('wordnet')
-class Topic_Allocate():
-  def __init__(self):
-    self = self
-  def lemmertize(self,texts):
-   #texts type: list of string
+def lemmertize(texts):
+   #texts input type: list of string
    wordnet_lemmatizer = nltk.stem.WordNetLemmatizer()
    lemmertize_texts = []
    
    for text in texts:
     words = preprocess(text).split(' ')
     lemmertize_texts.append(' '.join([wordnet_lemmatizer.lemmatize(word,pos ='v') for word in words]))
+
+   #return lemmertized texts
    return lemmertize_texts
-   
-  def bagOfWords (self, text_data):
-    #convert to list of string
-    texts = self.lemmertize(text_data)
 
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(texts)
+class Topic_Allocate():
+  def __init__(self):
+    self = self
 
-    # create dictionary
-    self.dictionary = list(tokenizer.word_index.keys())
+  def cbow_fit (self, text_data, window_size):
+    texts = text_data
+    #split into words
+    texts = [text.split() for text in texts]
 
-    # create bag of words
-    self.bow = tokenizer.texts_to_matrix(texts, mode = 'count')
-  
-  def cbow_fit (self, text_data, window_size, vector_size):
-    self.vector_size = vector_size
-
-    #convert to list of string
-    texts = self.lemmertize(text_data)
-
-    for i in range(len(texts)):
-      texts[i] = texts[i].split()
-
+    #embeddind words
     word2vec = Word2Vec(texts, min_count = 1, window =  window_size, vector_size= self.vector_size)
 
     # create dictionary
     self.dictionary = list(word2vec.wv.key_to_index)
-
-    # self.w2v['minh'] = [..., ..., ...]
     self.w2v = word2vec.wv
     
+  def doc2vec (self, text_data, window_size = 4, vector_size = 200):
+    self.vector_size = vector_size
 
-  
-  def doc2vec (self, text_data):
-    #convert to list of string
-    texts = self.lemmertize(text_data)
+    #lemmertize texts 
+    texts = lemmertize(text_data)
 
+    #encode vocabulary to vectors
+    self.cbow_fit(texts, window_size)
+   
+    #transform texts into matrixs
     ts2vec = []
-
     for text in texts:
-      text2vec = [0] * self.vector_size
-      for word in text:
-        try:
-          text2vec += self.w2v[word]
-        except KeyError:
-          continue
+      sentenes = text.split('.')
+      text2vec = np.empty((len(sentenes),self.vector_size))  
+      for idx, sent in enumerate(sentenes):
+        sen2vec = np.zeros((1, self.vector_size))
+        for word in sent:
+            try:
+                sen2vec += self.w2v[word]
+            except KeyError:
+                continue
+        text2vec[idx] = sen2vec
       ts2vec.append(text2vec)
-      
     return ts2vec
